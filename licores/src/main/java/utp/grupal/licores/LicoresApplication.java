62d7c34bdef849fb5e30dfa6641d0296f191cb6c
Gustavasho <expectra2010@gmail.com>
2021-04-18 13:54:58 -0500

    first commit

---
+++ licores/src/main/java/utp/grupal/licores/LicoresApplication.java
@@ -0,0 +1,13 @@
+package utp.grupal.licores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicoresApplication.class, args);
+	}
+
+}
---
+++ licores/src/test/java/utp/grupal/licores/LicoresApplicationTests.java
@@ -0,0 +1,13 @@
+package utp.grupal.licores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
